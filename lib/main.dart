289dcc7911cb6f5bc221b997fa555e7e09c926b7
ahmad-cod <ahmadadewale800@gmail.com>
2024-05-05 21:40:47 +0100

    Animate the initial page shown after some seconds

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/screens/intro_screen.dart';
+import 'package:whatsapp_clone/screens/layout.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Widget _home = const IntroScreen();
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 9), () {
+      setState(() {
+        _home = const HomeScreen();
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal[900]!)
+      ),
+      home: _home,
+    );
+  }
+}
