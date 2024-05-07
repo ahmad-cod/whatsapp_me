3be2cc533c8943fbaed3e7a553b118a909c9810e
ahmad-cod <ahmadadewale800@gmail.com>
2024-05-08 00:16:25 +0100

    Add a database for contacts

---
+++ lib/data/contacts_db.dart
@@ -0,0 +1,35 @@
+import 'package:whatsapp_clone/models/contact.dart';
+
+List<Contact> contactList = const [
+  Contact(
+    name: 'Jalasem',
+    imagePath: 'jalasemdp.jpeg',
+    about: 'I hate spaghetti code'),
+  Contact(
+    name: 'Code Village',
+    imagePath: 'codeVillage.jpg',
+    about: 'We build world class Tech Talent'),
+  Contact(
+    name: 'E. Musk',
+    imagePath: 'spaceX.png',
+    about: 'launches advanced rockets',),
+  Contact(
+    name: 'Ahmad',
+    imagePath: 'ahmadDp.png',
+    about: 'Sincerity, Excellence and Service'),
+  Contact(
+    name: 'الشيخ يونس',
+    imagePath: 'palestine.jpeg'),
+  Contact(
+    name: 'Maafy',
+    imagePath: 'maafy.jpeg',
+    about: 'Veiled Pharmacist'),
+  Contact(
+    name: 'Rentlify Team',
+    imagePath: 'rentlify.jpeg',
+    about: 'Simplifying house renting'),
+  Contact(
+    name: 'Scholar AR',
+    imagePath: 'dekunle.jpeg',
+    about: 'A scholar at the American University Kenya'),
+];
