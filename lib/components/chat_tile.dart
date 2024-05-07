9ad739f7d1997167553bac7912981b899f09ad59
ahmad-cod <ahmadadewale800@gmail.com>
2024-05-07 11:05:02 +0100

    Enable navigation to individual conversations

--- lib/components/chat_tile.dart
+++ lib/components/chat_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/models/chat.dart';
+import 'package:whatsapp_clone/screens/chat_screen.dart';
 
 class ChatTile extends StatelessWidget {
   const ChatTile({super.key, required this.chat});
@@ -8,25 +9,31 @@ class ChatTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Container(
-        decoration: BoxDecoration(
-          color: Colors.teal,
-          borderRadius: BorderRadius.circular(28)
+    return GestureDetector(
+      onTap: () => Navigator.push(context,
+      MaterialPageRoute(
+        builder: (context) => ChatView(chatUser: chat),
+      )),
+      child: ListTile(
+        leading: Container(
+          decoration: BoxDecoration(
+            color: Colors.teal,
+            borderRadius: BorderRadius.circular(28)
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(25),
+            child: Image.asset( 
+              chat.imagePath, width: 45, height: 45, ),
+          ),
         ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(25),
-          child: Image.asset( 
-            chat.imagePath, width: 45, height: 45, ),
+        title: Text(
+          chat.name,
+          style: const TextStyle(color: Colors.white),
         ),
-      ),
-      title: Text(
-        chat.name,
-        style: const TextStyle(color: Colors.white),
-      ),
-      subtitle: Text(
-        chat.message,
-        style: TextStyle(color: Colors.grey[350]),
+        subtitle: Text(
+          chat.message,
+          style: TextStyle(color: Colors.grey[350]),
+        ),
       ),
     );
   }
